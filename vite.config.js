3192fc1d4b8c1d77851887c3ba4e9a41038b7d56
Luisen45 <enriquecastanon54@gmail.com>
2025-05-17 19:12:23 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Oceloteapp/',   // ← ¡Pon aquí el nombre EXACTO de tu repositorio entre las barras!
+  base: '/Oceloteapp/',   // ← Nombre EXACTO del repo
   plugins: [react()],
 })
